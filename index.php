00de44d50bb138d0eb8cdb047cefbc346c4ae1bf
Samantha <samanthamccallfp18.com>
2019-10-15 19:17:28 -0500

    adding some text so I can push new app live

--- index.php
+++ index.php
@@ -1,22 +1,37 @@
+
 <?php
 
    // this is a constant that can't be overridden. note ALL CAPS
-   define('NAME', 'Yoshi');
+  //  define('NAME', 'Yoshi');
 
-   $stringOne = "my email is ";
-   $stringTwo = "samanthamccall@gmail.com";
+  //  $stringOne = "my email is ";
+  //  $stringTwo = "samanthamccall@gmail.com";
 
-   $name = 'Yoshi';
+  //  $name = 'Yoshi';
 
    // \ is an escape character
-   echo "Hey my name is $name and I say \"hooray\"";
+  //  echo "Hey my name is $name and I say \"hooray\"";
 
 
   // $age = 30;
+  // $pi  = 3.14;
+  // $radius = 25;
+  // echo $pi * $radius^2;
 
-echo strlen($name);
-echo str_replace('y', 'w', $name);
+  // echo 2 * (4 + 9) / 3;
 
+  // increments and decrement operations
+
+  // echo $radius++; //++ is the same as +1
+  // echo $radius;
+
+  // $age = 20;
+  // $age += 10;
+
+// number functions
+// echo floor($pi);
+// echo ceil($pi);
+// echo pi();
 
 ?>
 <!DOCTYPE html>
@@ -25,12 +40,17 @@ echo str_replace('y', 'w', $name);
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>My first PHP file</title>
+  <title>Welcome to My First PHP App</title>
 </head>
 <body>
-  <h1>User Profile Page</h1>
-  <div><?php echo NAME; ?></div>
-  <div><?php echo $age; ?></div>
+<div>
+<h1>Welcome to My First PHP App</h1>
+<p>Now that this page has a <a href="https://github.com/green64/php_project" target="_blank">Github repo</a> and a <a href="https://agile-wildwood-45625.herokuapp.com/" target="_blank">Heroku app</a> to take advantage of its 
+MariaDB (what a great tool XAMPP has turned out to be!), I'm back to building my first PHP app.</p> 
+<p>Watch this space to see what I create!</p></div>
 
+  
 </body>
 </html>
+
+
