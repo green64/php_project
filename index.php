c304656d1aaa37b2f6519a00ac5d4cdade12ca6d
Samantha <samanthamccallfp18.com>
2019-10-15 19:31:09 -0500

    adding some text so I can push new app live

--- index.php
+++ index.php
@@ -43,11 +43,16 @@
   <title>Welcome to My First PHP App</title>
 </head>
 <body>
-<div>
+<div style="margin: 40px;"> 
 <h1>Welcome to My First PHP App</h1>
 <p>Now that this page has a <a href="https://github.com/green64/php_project" target="_blank">Github repo</a> and a <a href="https://agile-wildwood-45625.herokuapp.com/" target="_blank">Heroku app</a> to take advantage of its 
 MariaDB (what a great tool XAMPP has turned out to be!), I'm back to building my first PHP app.</p> 
-<p>Watch this space to see what I create!</p></div>
+
+<p>I've already learned about PHP differences, such as needing a composer.json file instead of a package.json to successfully 
+host the app in Heroku. I've also discovered similarities, such as familiar operators, arrays, variables, and more. I'm excited that I'm able to transition to PHP
+fairly easily, thanks to my existing full-stack knowledge. And even more excited to see what's next.</p>
+
+<p>Watch this space for something great!</p></div>
 
   
 </body>
